7d9e4264b116ab7df5d97ec6909a8720630048e8
zthurman <zrt81491@gmail.com>
2015-09-16 18:12:59 -0700

    cleaning the imports up

--- Songscape/Songscape.py
+++ Songscape/Songscape.py
@@ -1,15 +1,16 @@
-
 #!/usr/bin/env python
-#  Songscape.py
-## This is a program that will take an audio file input and generate a real-time 3D spectrogram of the audio file.
+#  Songscape
+
+# This is a program that will take an wav audio file input and
+# generate a real-time 3D spectrogram of the audio file.
+
+# Copyright (C) 2015 Zechariah Thurman
+# GNU GPLv2
 
 from __future__ import division
 from scipy.io import wavfile
 import scipy as sc
 from numpy import *
-import numpy as np
-import pylab
-import matplotlib as mp
 from matplotlib import pyplot as plt
 import sys
 
