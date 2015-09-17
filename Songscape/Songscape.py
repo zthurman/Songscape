df7ef87c6975fa0b95d493f7dd63794c1d34cfb2
zthurman <zrt81491@gmail.com>
2015-09-16 18:06:34 -0700

    now the spectrogram spectroanalyzes, beware as of this commit it takes a LOOOOOOOOOOOOOOOOOOOOOOOOONG time to run

--- Songscape/Songscape.py
+++ Songscape/Songscape.py
@@ -1,6 +1,8 @@
+
 #!/usr/bin/env python
 #  Songscape.py
 ## This is a program that will take an audio file input and generate a real-time 3D spectrogram of the audio file.
+
 from __future__ import division
 from scipy.io import wavfile
 import scipy as sc
@@ -13,14 +15,9 @@ import sys
 
 fs, data = sc.io.wavfile.read('/home/zechthurman/Songscape/03. Kali 47 (Original Mix).wav')
 t = data.size
+NFFT = 1024
+Fs = int(1.0/dt)  # the sampling frequency
+sh
 
-def do_spectrogram(x):
-    plt.specgram(x, NFFT=256, Fs=2, Fc=0, detrend=mlab.detrend_none,
-         window=mlab.window_hanning, noverlap=128,
-         cmap=None, xextent=None, pad_to=None, sides='default',
-         scale_by_freq=None, mode='default', scale='default',
-         **kwargs)
-
-print do_spectrogram(data)
-
-
+plt.specgram(data[:,1], NFFT=NFFT, Fs=Fs, noverlap=900,
+                            cmap=cm.gist_heat)
