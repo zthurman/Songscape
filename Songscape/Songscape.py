b011a94c27099e08eebd8071f9a1330976cc2c1e
zthurman <zrt81491@gmail.com>
2015-09-16 18:16:05 -0700

    computer needs a restart

--- Songscape/Songscape.py
+++ Songscape/Songscape.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#  Songscape
+# Songscape
 
 # This is a program that will take an wav audio file input and
 # generate a real-time 3D spectrogram of the audio file.
@@ -18,9 +18,10 @@ import sys
 def spectrogram():
     fs, data = sc.io.wavfile.read('/home/zechthurman/Songscape/03. Kali 47 (Original Mix).wav')
     t = data.size/500
+    dt = 0.0005
     NFFT = 1024
     Fs = int(1.0/dt)  # the sampling frequency
 
     plt.specgram(data[:,1], NFFT=NFFT, Fs=Fs, noverlap=900,
-                                cmap=cm.gist_heat)
-
+                                cmap=gist_heat)
+spectrogram()
